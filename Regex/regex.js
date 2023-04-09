3a8d81f87b80b122eb989fc6895f1a9374cbc35a
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-04-10 00:04:56 +0300

    Add files via upload

--- Regex/regex.js
+++ Regex/regex.js
@@ -28,18 +28,41 @@ I have 50$.
 I have 100€`
 
 const extractCurreny = (text) => {
-    let r = /([0-9]+[., ]?[0-9]+[ ]?)(USD|EUR|EURO|MDL|\$|\€)/g
-    const result = text.match (r)
+    let r = /([0-9]+[.,\s]?[0-9]+[\s]?)(USD|EUR|EURO|MDL|\$|\€)/g
+    const result = text.match(r)                   // ['1000.00 USD', '1000 00 USD', '1000 USD', '100.00EUR', '100.00 EUR', '50,50 MDL', '50$', '100€']
+    const filtered = result.filter( v => (v.includes("USD") || v.includes("$") ))
+    //const filtered1 = result.filter(v => ( v.endsWith("USD") || v.endsWith("$"))  )
+    const splitted = filtered.toString()
+    const converted = splitted.replace( /([0-9]+[.,\s]?[0-9]+[\s]?)(USD|\$)/g, (m,g1,g2) => { 
+        let amount = g1*0.92 
+        return `${amount}EUR`
+     } ).split(",")
     //const sorted = result.sort( (a,b)=> a.localeCompare(b))
-    return result
 
-    for( let i of result){
-    console.log(i)
+    for( let i of result){   
+        console.log(i)
     }
+    
+    //[...result].forEach(  v=> console.log(v) ) = o alternativa a for-lui standart
+
+    return [result,filtered,splitted, converted]
+}
+console.log(extractCurreny(text));
+
+
+
+
+
+const extractCurrency2 = (text) => {
+    let r = /([0-9]+[.,\s]?[0-9]+[\s]?)(USD|EUR|EURO|MDL|\$|\€)/g
+    let r_dollar = /([0-9]+[.,\s]?[0-9]+[\s]?)(USD|\$)/g
+    let matches = text.matchAll(r)
+    return [...matches].filter( v=> (v.includes("USD") || v.includes("$")))
+
 }
 
+console.log(extractCurrency2(text));
 
-console.log(extractCurreny(text));
 
 
 
@@ -68,24 +91,44 @@ console.log(extractCurreny(text));
 // write a script that extracts email addresses and phone numbers from a text
 
 let text1 = `My mail is: MARia_-.fasj19s@gmail.com with phone 063012548. Your email is: example@gmail.com with phone 063 01 25 48. His email is mario45._k@mail.ru with  phone nr: 0-693-215-66.Their emails are KristAl-d.2_A@yahoo.com with phone nr: +3736958956`
+//Array with phone separate
+// const extractPhone = (text1) => {
+//     let rp = /((\+373)?[ -])?(\d{1,3})[ -]?(\d{2,3})?[ -]?(\d{2,3})[ -]?(\d{2,3})[ -]?(\d{2,3})[ -]?/g
 
-const extractPhone = (text1) => {
+//     const phoneNumbers =  text1.match(rp)
+//     return phoneNumbers 
+// }
+// console.log(extractPhone(text1))
+
+
+//Array with separate emails
+// const extractMail = (text1) => {
+//     let rm = /[a-zA-Z0-9._\-]+[@][a-z]+\.[a-z]{2,3}/g
+
+//     const emails =  text1.match(rm)
+//     return emails 
+// }
+// console.log(extractMail(text1))
+
+
+
+
+//array of object with email-phone per user
+const extractInfo = (text1) => {
     let rp = /((\+373)?[ -])?(\d{1,3})[ -]?(\d{2,3})?[ -]?(\d{2,3})[ -]?(\d{2,3})[ -]?(\d{2,3})[ -]?/g
-
-    const phoneNumbers =  text1.match(rp)
-    return phoneNumbers 
-}
-console.log(extractPhone(text1))
-
-
-
-const extractMail = (text1) => {
     let rm = /[a-zA-Z0-9._\-]+[@][a-z]+\.[a-z]{2,3}/g
-
+    const phoneNumbers =  text1.match(rp)
     const emails =  text1.match(rm)
-     
-    return emails 
+    return emails.map( (email,index) => {
+        return {
+            email:email,
+            phone_number: phoneNumbers[index]
+        }
+    } )
+
+    //return [phoneNumbers, emails] 
 }
-console.log(extractMail(text1))
+
+console.log(extractInfo(text1))
 
 
