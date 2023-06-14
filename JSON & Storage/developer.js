626c2542052f7a39b12321bcc870c56efbe2e8f4
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-06-14 12:29:24 +0300

    Add files via upload

---
+++ JSON & Storage/developer.js
@@ -0,0 +1,24 @@
+class Developer {
+    constructor(name, experience, domains){
+        this.name = name,
+        this.experience = experience,
+        this.domains = domains
+    }
+
+    toJSON() {       
+        return JSON.stringify({
+            name: this.name,
+            experience : this.experience,
+            domains: this.domains
+        })
+    }
+
+    fromJSON(value){
+        return JSON.parse( value )
+
+    }
+
+
+}
+
+let dev1 = new Developer("John", 10, ["JS", "CSS"])
