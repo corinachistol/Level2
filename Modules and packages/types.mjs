b198a0d5042fa852eb180794c4c5377029f7289a
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-08-01 07:55:50 +0300

    Add files via upload

--- Modules and packages/types.mjs
+++ Modules and packages/types.mjs
@@ -3,24 +3,19 @@ import temperature from './temperature.json' assert { type:"json" }
 console.log(temperature)
 console.log(typeof temperature)
 
+const arr = temperature.temps
+console.log(arr)
 
-const isNumber = (obj) => {
-    for( let i=0; i < obj.length; i++){
-        if(typeof obj[i] == Array){
-            console.log(obj[i])
-            //return true
-            
-            let filtered = obj[i].filter( element =>{
-                typeof element !== Number
-            } )
-            return filtered
-        }
-                
-    }
-    // let filtered = obj.filter( element => {
-    //     typeof element !== Number
-    // } )
+
+const isNumber = (arr) => {
+    return arr.filter(element => typeof element == "number" )
+    // let filtered = arr.filter( (element, index) => {
+    //     if(typeof element !== Number){
+    //         arr.splice(element[index], )
+    //         console.log(arr)
+    //     }
+    // } ) 
     // return filtered
 }
 
-console.log(isNumber({ temperature }))
+console.log(isNumber(arr))
