37d57ecfe8e472eb0647b9c11eeaae6e79d84968
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-08-01 08:54:18 +0300

    Add files via upload

--- Modules and packages/hw-app.mjs
+++ Modules and packages/hw-app.mjs
@@ -1,1 +1,17 @@
-import temperature from './temperature.json' assert { type:"json" }
+
+import {arr, isNumber, onlyNumbers} from './types.js'
+import { add,divide, substract, multiply } from './math.js'
+
+//console.log(arr)
+
+console.log(onlyNumbers) // [ 100, -100, 0.5, 1 ]
+console.log(...onlyNumbers)  // 100 -100 0.5 1
+
+function avgTempValue(...onlyNumbers) {
+    let sum =  add(...onlyNumbers)
+    return sum
+   
+}
+console.log(avgTempValue(...onlyNumbers))
+
+//console.log( sum)
