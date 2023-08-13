9c777922b7e31d24f2e3df944ef54464a5304024
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-08-13 22:53:52 +0300

    Add files via upload

---
+++ Server-to-server-api/app2.mjs
@@ -0,0 +1,65 @@
+import http from 'node:http'
+import key from './config.json' assert { type: "json" }
+import * as readline from 'node:readline'
+import { stdin as input, stdout as output } from 'node:process';
+//console.log(key.KEY)
+
+let city = "Chisinau"
+
+const rl = readline.createInterface( { input, output } )
+
+// const enterCity = () => {
+//     rl.question('Enter a city name...', answer => {
+//         console.log(answer)
+//     })
+// }
+
+const options = {
+    host: key.HOST,
+    path: key.PATH + `?q=${city}&appid=${key.KEY}&units=metric`,
+    port: 80,
+    method: "GET"
+}
+
+//cream o functie care ne va returna orasul
+const getCityWeather = (cb) => {
+    const req = http.request(options, (response) => {
+        //console.log( `STATUS: ${response.statusCode}`)
+
+        if(response.statusCode !== 200){
+            console.log( `Wrong City Name: ${response.statusCode}: ${response.statusMessage}`)
+        }
+        
+        response.setEncoding('utf8')
+        let rawData = ""
+        response.on('data', (chunk)=>{ rawData += chunk})
+        response.on('end', ()=>{
+            const parsedData = JSON.parse(rawData)
+            cb(parsedData)
+            //console.log(parsedData)
+            // let {
+            //     main: {temp, temp_min, temp_max},
+            //     wind:{speed}
+            //   } = parsedData
+            //   console.log(`>>>>>> In ${city} city there are ${Math.round(temp)}C with a minimum temperature of ${Math.round(temp_min)} and maximum ${Math.round(temp_max)}. The speed of the wind is ${Math.round(speed)}km/h`)
+        })
+        response.on('error', error => console.log(error))
+    } )
+
+    req.end()
+   
+}
+
+const printCityWeather = (json) => {
+    let {
+        main: {temp, temp_min, temp_max},
+        wind:{speed}
+    } = json
+
+    console.log(`>>>>>> In ${city} city there are ${Math.round(temp)}C with a minimum temperature of ${Math.round(temp_min)} and maximum ${Math.round(temp_max)}. The speed of the wind is ${Math.round(speed)}km/h`)
+}
+
+
+//enterCity(getCityWeather(printCityWeather))
+
+getCityWeather(printCityWeather)
