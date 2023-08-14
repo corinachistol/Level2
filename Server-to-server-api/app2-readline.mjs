7bf168d78538badb1fb34caf788bf4aad47e0f40
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-08-14 15:30:52 +0300

    Add files via upload

--- Server-to-server-api/app2-readline.mjs
+++ Server-to-server-api/app2-readline.mjs
@@ -1,7 +1,7 @@
 import * as readline from 'node:readline'
 import { stdin as input, stdout as output } from 'node:process';
 
-//import { getCityWeather, printCityWeather } from './app2.mjs'
+import { getCityWeather, printCityWeather } from './app2.mjs'
 
 
 const rl = readline.createInterface( { input, output } )
@@ -13,6 +13,9 @@ const rl = readline.createInterface( { input, output } )
 
 export const getCityName = () => {
     rl.question('Enter a city name ... ', (answer) => {
+        getCityWeather((answer, ) => {
+            printCityWeather (answer)
+        })
         console.log(answer)
         
     })
